69f3ac2a1a7b3a2998fbbe6289f10e559274f4fc
Christian Weyer <christian.weyer@thinktecture.com>
2021-11-02 11:47:29 +0100

    Cleanup

--- GrpcCodeFirst.Api/Startup.cs
+++ GrpcCodeFirst.Api/Startup.cs
@@ -1,10 +1,8 @@
-using AutoMapper;
 using GrpcCodeFirst.Api.GrpcServices;
 using GrpcCodeFirst.Api.Model;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
